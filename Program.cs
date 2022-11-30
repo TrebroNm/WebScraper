e0d04997903a69a7bfdde7d85fa7b7872eef2878
TrebroNm <81372891+TrebroNm@users.noreply.github.com>
2022-11-30 16:08:05 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using HtmlAgilityPack;
+
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        var url = "https://www.timeanddate.no/vaer/?continent=europe&low=c";
+        var web = new HtmlWeb();
+        var doc = web.Load(url);
+
+        Console.WriteLine(doc.Text);
+    }
+}
